1844140b24c8f37a3ee90df15778941dec0e3a62
Damir <DMiniakhmetov@sberbank.ru>
2022-12-05 11:29:14 +0300

    init proj

---
+++ ARbuzApp/ARbuzApp/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  ARbuzApp
+//
+//  Created by Миниахметов Дамир on 05.12.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+	var window: UIWindow?
+
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		// Override point for customization after application launch.
+		return true
+	}
+}
+
